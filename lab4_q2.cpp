a28d64bb74b2fb3ffd073f6c2a614c04bbed5911
1raviprakash <42237336+1raviprakash@users.noreply.github.com>
2018-08-22 04:00:59 -0400

    Add files via upload

---
+++ lab4_q2.cpp
@@ -0,0 +1,18 @@
+//library
+#include <iostream>
+using namespace std;
+//main function
+int main ()
+{
+// fixing variables
+float C, F;
+// enter temp in celcius
+cout << "enter length in celcius" << endl;
+//store value in  C
+cin >> C ;
+// enter temp in fahrenheit
+F = (C*9/5)+32 ;
+// print in terminal
+cout << "temp in fahrenheit = " << F << endl;
+return 0;
+}
